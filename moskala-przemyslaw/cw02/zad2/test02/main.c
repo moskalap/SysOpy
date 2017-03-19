5ecc96786dae20b1535c41f261722e7e537d38e1
moskalap <executeorder66>
2017-03-19 21:26:39 +0100

    użyto nwfts

--- moskala-przemyslaw/cw02/zad2/test02/main.c
+++ moskala-przemyslaw/cw02/zad2/test02/main.c
@@ -1,12 +1,12 @@
+#define _XOPEN_SOURCE 500
+#include <ftw.h>
 #include <stdio.h>
-#include <unistd.h>
-
-#include <dirent.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
 #include <sys/types.h>
+#include <stdint.h>
 #include <time.h>
+#include <dirent.h>
 
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
@@ -15,6 +15,16 @@
 #define ANSI_COLOR_MAGENTA "\x1b[35m"
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
+#define DT_DIR      0x4
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 
 const char* SLASH = "/";
 
@@ -61,6 +71,7 @@ struct dirent* dent;
        if(dent->d_type==DT_DIR) search(root_path,dent->d_name, bytes);
         dent=readdir(root);
     }
+
     closedir(root);
 }
 void getPermission(char * root, char * file){
@@ -87,16 +98,58 @@ if(stat(root,&fileStat) < 0) printf("err");
 
 }
 
+
+
+
+static int display_info(const char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf){
+
+
+
+    printf( (S_ISDIR(sb->st_mode)) ? "d" : "-");
+    printf( (sb->st_mode & S_IRUSR) ? "r" : "-");
+    printf( (sb->st_mode & S_IWUSR) ? "w" : "-");
+    printf( (sb->st_mode & S_IXUSR) ? "x" : "-");
+    printf( (sb->st_mode & S_IRGRP) ? "r" : "-");
+    printf( (sb->st_mode & S_IWGRP) ? "w" : "-");
+    printf( (sb->st_mode & S_IXGRP) ? "x" : "-");
+    printf( (sb->st_mode & S_IROTH) ? "r" : "-"); //read other
+    printf( (sb->st_mode & S_IWOTH) ? "w" : "-"); //write other
+    printf( (sb->st_mode & S_IXOTH) ? "x" : "-");
+
+    printf("\t");
+    char* t=ctime(&(sb->st_mtime));
+    t[strlen(t)-1]='\t';
+    printf( "%s", t);
+    printf("%u bytes\t", (unsigned int)sb->st_size);
+
+    printf("\t %-40s \n",  fpath);
+    return 0;           /* To tell nftw() to continue */
+}
+
+void search_nwfd(int argc, char *path){
+    int flags = 0;
+    flags |= FTW_DEPTH;
+    flags |= FTW_PHYS;
+
+    if (nftw(path, display_info, 20, flags)
+        == -1) {
+        perror("nftw");
+        exit(EXIT_FAILURE);
+    }
+    exit(EXIT_SUCCESS);
+}
+
+
+
 int main(int argc, char* argv[]){
 
-    //const char * root_path=argv[1];
-   // const int max_bytes=atoi(argv[2]);
-    char* pah="/home/przemek/Dokumenty/Semestr II/pi";
+
+    char* pah=argv[1];
     char a[1024];
     strcpy(a,pah);
     //getPermission(pah);
-    search (a,NULL, 2);
-
+    //search (a,NULL, 2);
+    search_nwfd(2,pah);
 
 
 
