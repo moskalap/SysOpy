eca438da6396cfe6bd9e60646ed5dcba13b5490f
moskalap <executeorder66>
2017-04-12 22:13:08 +0200

    działa dla jednej lini

--- moskala-przemyslaw/cw05/zad1/interpreter.c
+++ moskala-przemyslaw/cw05/zad1/interpreter.c
@@ -1,7 +1,6 @@
 //
 // Created by przemek on 05.04.17.
 //
-int actual = 0;
 
 #include "interpreter.h"
 
@@ -9,9 +8,8 @@ void sig_handler(int n) {
     //Just a handler for interupt pause();
     return;
 }
-
 /**
- * Creates an arguments as array of strings.
+ * Creates a array of arguments to execvp.
  * @param buff buffer of chars betwen '|'
  * @return array of args
  */
@@ -46,7 +44,7 @@ Executable *create_executable(char *token) {
 }
 
 /**
- * displays content of executable struct
+ * displays content of executable structure
  * @param e
  */
 void display_executable(Executable *e, int fd[]) {
@@ -63,7 +61,7 @@ void display_executable(Executable *e, int fd[]) {
 
 /**
  * executes a single token
- * @param pipe
+ * @param executable struct
  */
 void exec_token(Executable *ex) {
     if (execvp(ex->args[0], ex->args) == -1)
@@ -97,21 +95,11 @@ void add_executable_to_pipe(Pipe *pipe, Executable *e) {
 
 }
 
-void execute_without_redir(Pipe *pipe) {
-    Executable *tmp = pipe->first;
-
-    for (int i = 0; i < pipe->n; i++) {
-        pid_t id = fork();
-        if (id == 0) {
-            exec_token(tmp);
-            exit(0);
-        } else {
-            waitpid(id, NULL, NULL);
-        }
-        tmp = tmp->output;
-    }
-}
-
+/**
+ * Builds a structure Pipe from given arguments
+ * @param args - string of tokens separated by "|"
+ * @return struct Pipe
+ */
 Pipe *build_from_args(char *args) {
     Pipe *pipe = create_pipe();
     char *tmp;
@@ -127,16 +115,16 @@ Pipe *build_from_args(char *args) {
 
 
     }
-
     for (int j = 0; j < i; j++) {
         add_executable_to_pipe(pipe, create_executable(tokens_list[j]));
     }
-
-
     return pipe;
 }
 
-
+/**
+ * Executes tasks in Pipe, redirecting k STDOUT to k+1 STDIN
+ * @param p - structure Pipe with Executables
+ */
 void execute_all(Pipe *p) {
     Executable *task = malloc(sizeof(Executable));
     task->input = p->last;
@@ -147,47 +135,51 @@ void execute_all(Pipe *p) {
     pipe(fd);
 
     create:
-    n--;
-    if (task)
-        task = task->input;
-    pid = fork();
-    if (pid == 0) goto child;
-    else goto parent;
+        n--;
+        if (task)
+            task = task->input;
+        pid = fork();
+        if (pid == 0) goto child;
+        else goto parent;
 
     parent:
-    if (getpid() == START_PROCESS_PID) goto interpreter;
-    dup2(fd[0], STDIN_FILENO);
-    close(fd[1]);
-    pause(); // Wait for a SIGALARM from child ( permission to start own task )
-    wait(NULL);
-    kill(getppid(), SIGALRM);  // send permission to parent
-    display_executable(task, fd);
-    exec_token(task);
-    exit(0);
+        if (getpid() == START_PROCESS_PID) goto interpreter;
+        dup2(fd[0], STDIN_FILENO);
+        close(fd[1]);
+        pause(); // Wait for a SIGALARM from child ( permission to start own task )
+        wait(NULL);
+        kill(getppid(), SIGALRM);  // send permission to parent
+        display_executable(task, fd);
+        exec_token(task);
+        exit(0);
 
 
     child:
-    if (getppid() != START_PROCESS_PID) dup2(fd[1], STDOUT_FILENO);
-    close(fd[0]);
+        if (getppid() != START_PROCESS_PID) dup2(fd[1], STDOUT_FILENO);
+        close(fd[0]);
 
-    if (n >= 1) goto pipe;
-    else kill(getppid(), SIGALRM);
+        if (n >= 1) goto pipe;
+        else kill(getppid(), SIGALRM);
 
 
     interpreter:
-    wait(NULL);
-    exit(0);
+        wait(NULL);
+        return;
 
 }
 
 
 int main(int argc, char *argv[]) {
+    int BFR_LEN=200;
     signal(SIGALRM, sig_handler);
     signal(SIGCHLD, sig_handler);
     START_PROCESS_PID = getpid();
-
-    char buf[] = "ls -la | grep Makefile | wc | wartownik";
-    Pipe *p = build_from_args(buf);
+    char buf[BFR_LEN];
+    //char buf[] = "ls -la | grep Makefile | wc | wartownik";
+    Pipe *p;
+    fgets(buf,BFR_LEN,stdin);
+    fgets(buf,BFR_LEN,stdin);
+    p = build_from_args(buf);
 
     execute_all(p);
 
