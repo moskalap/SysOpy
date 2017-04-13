616693a3b76cce3702e6106eb8c83b4908121888
moskalap <executeorder66>
2017-04-13 14:03:57 +0200

    slave pisze do pliku

--- moskala-przemyslaw/cw05/zad2/slave.c
+++ moskala-przemyslaw/cw05/zad2/slave.c
@@ -7,28 +7,70 @@
 #include <stdlib.h>
 #include <time.h>
 #include "slave.h"
-#include <complex.h>
-double complex generate_complex(){
-    int x=rand()%((RE_MAX-RE_MIN)*100);
-    int y=rand()%((IM_MAX-IM_MIN)*100);
-    double xd = (double) x/10000.0+RE_MIN;
-    double yd;
-    return xd + yd * I;
+#include<stdio.h>
+#include<complex.h>
+#include<math.h>
+#include <zconf.h>
+#include <string.h>
+
+Point generate_point(){
+    int x = rand()%(RE_MAX-RE_MIN)+RE_MIN;
+    int y = rand()%(IM_MAX-IM_MIN)+IM_MIN;
+    Point p;
+    p.x=x/100.0;
+    p.y=y/100.0;
+    p.abs = sqrt(p.x*p.x + p.y*p.y);
+    p.sina = p.y/p.abs;
+    p.cosa = p.x/p.abs;
+
+    return p;
+
+}
+void display_complex(Point p){
+    printf("%f+i*(%f) iter: %d\n",p.x,p.y,p.iter);
+    //printf("%f+i*(%f) = |%f|(%f +i%f)\n",p.x,p.y,p.abs,p.cosa,p.sina);
+}
+
+int count_iter(Point p, int max){
+    int i=0;
+    double complex zn = 0.0;
+    double complex c=p.x+p.y*I;
+    for(i=0; i<max && cabs(zn)<2.0; i++){
+        zn=cpow(zn, 2.0)+c;
+    }
+    return i;
+
 
 }
 int main(int argc, char* argv[]){
     srand(time(NULL));
+    char buffer[50];
     if (argc<4) printf("usage: slave [path to named pipe] [amount of complex number to generate] [max limit of iterations]\n");
     else{
-        int pipe = open(argv[1], O_WRONLY);
+        char PATH[100];
+        strcpy(PATH,argv[1]);
+        int pipe=open("smiesznyplik.txt", O_CREAT|O_APPEND|O_WRONLY,S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH | S_IWOTH);
+        if(pipe<0) {
+            fprintf(stderr, "error while opening pipe\n");
+            EXIT_FAILURE;
+        }
+
+
         int N=atoi(argv[2]);
         int K=atoi(argv[3]);
         int iter, j;
-        double complex rand;
+        Point rand;
+        double complex com;
+
+
         for (j = 0; j<N; j++){
-            rand = generate_complex();
-            iter = count_iter(g);
-            write_to_pipe(pipe,rand,iter);
+
+            rand = generate_point();
+            rand.iter=count_iter(rand,100);
+           // display_complex(rand);
+            sprintf(buffer,"%lf %lf %d\n",rand.x,rand.y,rand.iter);
+            //printf(buffer);
+            write(pipe, buffer,50);
         }
 
     }
--- moskala-przemyslaw/cw05/zad2/slave.h
+++ moskala-przemyslaw/cw05/zad2/slave.h
@@ -5,12 +5,18 @@
 #ifndef CW05Z02_SLAVE_H
 #define CW05Z02_SLAVE_H
 
-#include <stdio.h>
-#include <complex.h>
 
-const int IM_MAX = 1;
-const int IM_MIN = -1;
-const int RE_MAX = 1;
-const int RE_MIN = -2;
+typedef struct Point{
+    double x;
+    double y;
+    double abs;
+    double sina;
+    double cosa;
+    int iter;
+}Point;
+const int IM_MAX = 100;
+const int IM_MIN = -100;
+const int RE_MAX = 100;
+const int RE_MIN = -200;
 
 #endif //CW05Z02_SLAVE_H
