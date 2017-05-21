b0e76d07d256874b99a5f67b00d0724329ae3a3b
moskalap <przemyslaw.moskala@gmail.com>
2017-05-21 13:23:50 +0200

    działa dla 1 x reader 1 x  writer

--- moskala-przemyslaw/cw09/zad1/reader_writer_posix.c
+++ moskala-przemyslaw/cw09/zad1/reader_writer_posix.c
@@ -3,28 +3,73 @@
 //
 
 #include <time.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <pthread.h>
+#include <zconf.h>
 #include "reader_writer_posix.h"
 
+int read_count = 0;
 int READERS = 0;
 int WRITERS = 0;
-int verbose = 0;
+int verbose = 1;
 int N = 0;
 int *T;
+sem_t *res_sem;
+sem_t *r_sem;
+sem_t *w_sem;
+int run = 1;
+
+void end_fun(int a) {
+    run = 0;
+}
+
+void sem_post_er(sem_t *a) {
+    if (-1 == sem_post(a)) {
+        perror("sempost");
+        kill(getpid(), SIGINT);
+    }
+}
+
+void sem_wait_er(sem_t *a) {
+    if (-1 == sem_wait(a)) {
+        perror("semwait");
+        kill(getpid(), SIGINT);
+    }
+}
 
 int main(int argc, char *argv[]) {
     srand(time(NULL));
-
+    signal(SIGINT, end_fun);
     parse_args(argc, argv);
+    res_sem = make_semaphore("rw_sem");
+    //w_sem=make_semaphore("w_sem");
+    r_sem = make_semaphore("r_sem");
     T = malloc(N * sizeof(int));
+    pthread_t reader;
+    pthread_t writer;
+    int a = 1;
+    pthread_create(&reader, NULL, &do_reader_job, &a);
+    pthread_create(&writer, NULL, &do_writer_job, NULL);
 
 
+    pthread_join(reader, NULL);
+    pthread_join(writer, NULL);
+
+
+    destroy_semaphore(res_sem, "rw_sem");
+    destroy_semaphore(r_sem, "r_sem");
+    // destroy_semaphore(w_sem, "w_sem");
 }
+
 
 void do_writer_job(void *arg) {
     int mods;
     int pos;
     int val;
-    while (1) {
+    while (run) {
+        sem_wait_er(res_sem);
         mods = rand() % N;
         for (int i = 0; i < mods; i++) {
             pos = rand() % N;
@@ -33,6 +78,8 @@ void do_writer_job(void *arg) {
             if (verbose)
                 printf("writer:\t T[%d]=%d\n", pos, val);
         }
+
+        sem_post_er(res_sem);
     }
 
 }
@@ -41,7 +88,14 @@ void do_reader_job(void *arg) {
 
     int mod = (int) arg;
     int found = 0;
-    while (1) {
+    while (run) {
+        sem_wait(r_sem);
+        read_count++;
+        if (read_count == 1)
+            sem_wait(res_sem);
+        sem_post_er(r_sem);
+
+
         found = 0;
         for (int i = 0; i < N; i++) {
 
@@ -52,6 +106,12 @@ void do_reader_job(void *arg) {
             }
         }
         printf("reader\tFound %d numbers", found);
+
+        sem_wait(r_sem);
+        read_count--;
+        if (read_count == 0)
+            sem_post_er(res_sem);
+        sem_post_er(r_sem);
     }
 
 }
@@ -64,6 +124,28 @@ void parse_args(int argc, char *argv[]) {
     } else {
         printf("usage: ./reader_writer_posix READERS WRITERS N");
         exit(0);
+    }
+}
+
+sem_t *make_semaphore(char *a) {
+
+    sem_t *sem = sem_open(a, O_CREAT | O_EXCL | O_RDWR, 0666, 1);
+    if (sem == SEM_FAILED) {
+        perror("init sem error");
+        // exit(-1);
+    }
+
+    return sem;
+}
+
+void destroy_semaphore(sem_t *a, char *b) {
+    if (0 != sem_close(a)) {
+        perror("destroy sem error");
+        // exit(-1);
+    }
+    if (0 != sem_unlink(b)) {
+        perror("destroy sem error");
+        exit(-1);
     }
 }
 
